43b437d0b58cf1ac5b0b68d0501900bc20107cc8
Hugo Ferreira Matos <mattosfmfdev@gmail.com>
2024-10-26 18:58:12 -0300

    Atualização do Front-end

--- scripts/acessibilidade/modo-escuro.js
+++ scripts/acessibilidade/modo-escuro.js
@@ -1,6 +1,26 @@
 const toggleButton = document.getElementById('ClaroEscuro');
 const body = document.body;
 
+function toggleIcon() {
+  const icon = document.getElementById("icon");
+  const body = document.body;
+
+  body.classList.toggle("dark");
+
+  if (icon.src.includes("caneta.png")) {
+    icon.src = "./style/assets/contrato.png";
+  } else {
+    icon.src = "./style/assets/caneta.png";
+  }
+
+  if (imagemHome.src.includes("homeIcon2.png")) {
+    imagemHome.src = "./style/assets/homeIcon.png";
+  } else {
+    imagemHome.src = "./style/assets/homeIcon2.png";
+  }
+
+}
+
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
 });
